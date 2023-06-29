7e6602945220076442076ec58248fb550597a91a
ajvar <cueille@protonmail.com>
2023-06-29 16:08:51 +0200

    Translated components

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,11 +9,28 @@ export const ui = {
   en: {
     "nav.home": "Home",
     "nav.about": "About",
-    "nav.work" : "Work"
+    "nav.work" : "Work",
+    "Send Me a Message" : "Send Me a Message",
+    "Interested in working together?" : "Interested in working together?",
+    "Proficient from the back to the front." : "Proficient from the back to the front.",
+    "Edge technologies" : "Edge technologies",
+    "Want to try something new ? I can help you implement the latest technos (exploring Qwik atm)." : "Want to try something new ? I can help you implement the latest technos (exploring Qwik atm).",
+    "Strategy-Minded" : "Strategy-Minded",
+    "Analytical and forward-thinking, adept at assessing complex situations, identifying opportunities, and devising effective long-term plans." : "Analytical and forward-thinking, adept at assessing complex situations, identifying opportunities, and devising effective long-term plans."
   },
   fr: {
     "nav.home": "Accueil",
     "nav.about": "À propos",
-    "nav.work" : "Projets",
+    "nav.work" : "Projets,",
+    "Send Me a Message" :"Contactez Moi",
+    "Interested in working together?" : "Vous souhaitez qu'on travaille ensemble ?",
+    "Proficient from the back to the front." : "Experience en frontend et backend",
+    "Edge technologies" : "Technologies modernes",
+    "Want to try something new ? I can help you implement the latest technos (exploring Qwik atm)." : "Envie de tester de nouvelles choses ? Je vous aide à implémenter des technologies modernes. J'explore Qwik en ce moment !",
+    "Strategy-Minded" : "Analyse de vos besoins",
+    "Analytical and forward-thinking, adept at assessing complex situations, identifying opportunities, and devising effective long-term plans." : "Analytique et tourné vers l'avenir, compétent pour évaluer des situations complexes, identifier des opportunités et concevoir des plans efficaces à long terme."
+
+
+
   },
 } as const;
